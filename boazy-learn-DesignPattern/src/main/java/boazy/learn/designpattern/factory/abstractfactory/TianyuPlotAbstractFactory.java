92f61c742cdf050f6bc7e18745bb975a4acdf8ff
sinoboazy <boazy@126.com>
2019-03-06 23:40:06 +0800

    复习工厂模式Factory Pattern

--- boazy-learn-DesignPattern/src/main/java/boazy/learn/designpattern/factory/abstractfactory/TianyuPlotAbstractFactory.java
+++ boazy-learn-DesignPattern/src/main/java/boazy/learn/designpattern/factory/abstractfactory/TianyuPlotAbstractFactory.java
@@ -6,6 +6,11 @@ import boazy.learn.designpattern.factory.IPropertyServiceCentre;
 /**
  * 抽象工厂模式（天誉花园）
  *
+ * 违背开闭原则，添加一个道口把有之前的实现都要修改代码实现新添加的
+ * 提供一个接口，用于创建相关或依赖对象的家族，而不需要明确指定具体类
+ *
+ * 规定了所有可能被创建的产品集合，产品族中扩展新的产品困难，需要修改抽象工厂的接口；增加了系统的抽象性和理解难度
+ *
  * @author boazy
  * @company boazy
  * @date 2018/8/5
--- boazy-learn-DesignPattern/src/main/java/boazy/learn/designpattern/factory/simplefactory/HouseSimpleFactory.java
+++ boazy-learn-DesignPattern/src/main/java/boazy/learn/designpattern/factory/simplefactory/HouseSimpleFactory.java
@@ -8,12 +8,21 @@ import boazy.learn.designpattern.factory.IHouse;
 /**
  * 简单工厂（房子简单工厂）
  *
+ * 违背开闭原则，每添加一个产品（一栋房子）都会导致修改工厂类
+ * 工厂类的职责相对过重，不易于扩展过于复杂的产品结构
+ *
  * @author boazy
  * @company boazy
  * @date 2018/8/5
  */
 public class HouseSimpleFactory {
 
+    /**
+     * 传入参数还可以是 className 或 class，这个创建方法不用作if判断直接通过反射创建实现
+     *
+     * @param type 类型
+     * @return type的IHouse实例
+     */
     public static IHouse createInstance(String type) {
         if("79".equalsIgnoreCase(type)) {
             return new House79Impl();
