19a37e13ffdc189386ff87097f2dc1a8b78a0f0a
sinoboazy <boazy@126.com>
2019-06-17 00:19:46 +0800

    add rpc4netty code

---
+++ rpc4netty/rpc4netty-protocol/src/main/java/com/github/sinoboazy/learn/rpc4netty/protocol/RpcBoazyProtocol.java
@@ -0,0 +1,36 @@
+package com.github.sinoboazy.learn.rpc4netty.protocol;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 自定义RPC传输协议（RpcBoazyProtocol）
+ *
+ * @author boazy
+ * @date 2019/6/17
+ */
+@Builder
+@Data
+public class RpcBoazyProtocol implements Serializable {
+    private static final long serialVersionUID = 3609633675201699157L;
+
+    /**
+     * 服务名（采用className）
+     */
+    private String serviceName;
+    /**
+     * 服务方法名
+     */
+    private String methodName;
+    /**
+     * 参数类型数组
+     */
+    private Class<?>[] argsClass;
+    /**
+     * 参数值数组
+     */
+    private Object[] args;
+
+}
